d78272bc5ff5d474b93be8058aed55148cc37df4
Timo Wilken <timo.wilken@cern.ch>
2022-11-07 15:44:39 +0100

    Only run CheckESC.C if we have AliPhysics
    CheckESD.C requires AliPhysics to run, but in CI it isn't always available.

--- test/gun/runtest.sh
+++ test/gun/runtest.sh
@@ -6,7 +6,10 @@ aliroot -b -q sim.C      2>&1 | tee sim.log
 mv syswatch.log simwatch.log
 aliroot -b -q rec.C      2>&1 | tee rec.log
 mv syswatch.log recwatch.log
-aliroot -b -q ${ALICE_ROOT}/STEER/macros/CheckESD.C 2>&1 | tee check.log
+if [ -n "$ALICE_PHYSICS" ]; then
+  # For this macro, we need OADB, which is part of AliPhysics.
+  aliroot -b -q "$ALICE_ROOT/STEER/macros/CheckESD.C" 2>&1 | tee check.log
+fi
 aliroot -b -q aod.C 2>&1 | tee aod.log
 
 cd recraw
