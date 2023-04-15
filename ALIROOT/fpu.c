41ebc9020e16600590a181a6b7f65445a4e1823b
Peter Hristov <pzhristov@users.noreply.github.com>
2023-04-15 09:44:22 +0300

    Fixes for Ubuntu 22.04. Do not link libraries loaded later (#1466)
    * Switch off trapfpe in case of problematic libxml2
    
    * Do not link libraries that are loaded later
    
    ---------
    
    Co-authored-by: hristov <peter.Hristov@cern.ch>

--- ALIROOT/fpu.c
+++ ALIROOT/fpu.c
@@ -1,4 +1,6 @@
 #ifdef __linux
+#include <libxml/xmlversion.h>
+#if LIBXML_VERSION < 20913
 #define _GNU_SOURCE 1
 #include <fenv.h>
 static void __attribute__ ((constructor)) trapfpe(void)
@@ -6,6 +8,9 @@ static void __attribute__ ((constructor)) trapfpe(void)
   /* Enable some exceptions. At startup all exceptions are masked. */
   feenableexcept(FE_INVALID|FE_DIVBYZERO|FE_OVERFLOW);
 }
+#else
+void trapfpe () {}
+#endif
 #else
 void trapfpe () {}
 #endif
